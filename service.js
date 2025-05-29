707adf37c7ab947c54ba291d3d8ca0d1a4eda56f
oyejide-olamide <oyejideolamide403@gmail.com>
2025-05-29 15:19:25 +0100

    Initial commit

---
+++ service.js
@@ -0,0 +1,9 @@
+
+const navMenu = document.getElementById('nav-menu');
+function openSidebar() {
+    navMenu.classList.add('open');
+}
+
+function closeSidebar() {
+    navMenu.classList.remove('open');
+}
